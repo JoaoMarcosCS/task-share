f1c3d3767a17837328e6363f12f634106eb312b7
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-02 10:24:52 -0300

    chore: environment util

---
+++ backend/src/shared/environment/env.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const envSchema = z.object({
+  APP_PORT: z.string(),
+  JWT_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRES: z.string(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_PORT: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
