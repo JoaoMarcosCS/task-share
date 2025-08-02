126c9167d5dce325941e23d1d583cb83fd5e3773
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-02 10:24:34 -0300

    feat: error handler middleware

---
+++ backend/src/infra/http/middleware/errorHandler.ts
@@ -0,0 +1,29 @@
+import { AppError } from "@shared/errors/errors";
+import { Request, NextFunction, Response } from "express";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+ 
+
+  if (err instanceof AppError) {
+    console.log("[errorHandler]:", err.message);
+    return res.status(err.statusCode).json({
+      error: {
+        type: err.name,
+        message: err.message,
+      },
+    });
+  }
+
+  console.error("Unhandled error:", err);
+  return res.status(500).json({
+    error: {
+      type: "InternalServerError",
+      message: "Internal server error",
+    },
+  });
+}
---
+++ backend/src/shared/errors/errors.ts
@@ -0,0 +1,41 @@
+import { ZodError } from "zod";
+
+export class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode = 400) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string) {
+    super(message, 409);
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(message: string) {
+    super(message, 500);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
