75a099663c5ac6b6307fa77cffaebe5a62ce7651
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-02 16:50:04 -0300

    fix: sintaxe

--- backend/src/infra/http/routes/user.routes.ts
+++ backend/src/infra/http/routes/user.routes.ts
@@ -6,11 +6,11 @@ export function UserRoutes(): Router {
   const router = Router();
   const controller = container.resolve(UserController);
 
-  router.post("/users", controller.create.bind(controller));
+  router.post("/", controller.create.bind(controller));
 
-  router.get("/users/by-email", controller.findByEmail.bind(controller));
-  router.get("/users/:id", controller.findById.bind(controller));
-  router.put("/users/:id", controller.update.bind(controller));
+  router.get("/by-email", controller.findByEmail.bind(controller));
+  router.get("/:id", controller.findById.bind(controller));
+  router.put("/:id", controller.update.bind(controller));
 
   return router;
 }
