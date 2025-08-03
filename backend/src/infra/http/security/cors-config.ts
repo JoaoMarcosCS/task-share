e27a6cbd04152aa7be517ecd48330c26329709ad
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-03 09:41:46 -0300

    feat: add cors protection

---
+++ backend/src/infra/http/security/cors-config.ts
@@ -0,0 +1,11 @@
+import { whiteList } from "./white-list";
+
+export const corsConfig = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Domínio não autorizado"));
+    }
+  },
+};
