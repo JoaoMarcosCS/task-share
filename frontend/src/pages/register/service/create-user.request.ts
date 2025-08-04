1674689b16b059dd253364fedcc0fd0eec977de5
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-04 10:29:36 -0300

    front/feat: register user

--- frontend/src/pages/register/service/create-user.request.ts
+++ frontend/src/pages/register/service/create-user.request.ts
@@ -1,8 +1,8 @@
-import { api } from "@/services/api";
+import { api } from "@/service/api";
 import type { CreateUserDTO } from "../schema/create-user.schema";
 
 const createUserRequest = async (body: CreateUserDTO) => {
-  const response = await api.post<boolean>(`/users`, body);
+  const response = await api.post<boolean>(`/lists`, body);
 
   return response.data;
 };
