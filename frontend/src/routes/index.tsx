58b4a1e69fd8618133196acdbb24adba531782b4
João Marcos <jmcsjoaomarcos@gmail.com>
2025-08-04 10:30:08 -0300

    front/chore: add routes pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,15 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Register } from "@/pages/register";
 import { Login } from "@/pages/login";
 import { useUserStore } from "@/store/user.store";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { Home } from "@/pages/home";
+import { LayoutPrivate } from "@/components/layout/LayoutPrivate";
+import { CreateTaskList } from "@/pages/create-task-list";
+import { TaskListDetails } from "@/pages/list-details/[listId]";
+import { CreateTask } from "@/pages/create-task";
+import { TaskListShares } from "@/pages/list-shares";
 
 const isAuthenticate = async () => {
   const accessToken = useUserStore.getState().user?.accessToken;
@@ -20,9 +25,30 @@ export const router = createBrowserRouter(
       errorElement: <ErrorBoundary />,
       children: [
         {
-          index: true,
-          element: <App />,
+          element: <LayoutPrivate />,
           loader: isAuthenticate,
+          children: [
+            {
+              index: true,
+              element: <Home />,
+            },
+            {
+              path: "/lists/new",
+              element: <CreateTaskList />,
+            },
+            {
+              path: "/lists/:listId",
+              element: <TaskListDetails />,
+            },
+            {
+              path: "/lists/:listId/tasks/new",
+              element: <CreateTask />,
+            },
+            {
+              path: "/lists/:listId/shares",
+              element: <TaskListShares />,
+            },
+          ],
         },
         {
           path: "register",
